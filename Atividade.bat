291ccd9d9a409aedf1917781f531c4b5eaf58b64
aliciasoares <aliciaoliveirasoaress@gmail.com>
2025-02-13 15:11:50 -0300

    Add files via upload

---
+++ Atividade.bat
@@ -0,0 +1,23 @@
+mkdir pasta1
+mkdir pasta2
+mkdir pasta3
+mkdir pasta4
+pause
+rmdir pasta1
+cd pasta2
+echo textotextotexto> Passarinho.txt
+pause
+rename Passarinho.txt Flor.txt
+cd ..
+move pasta3\Flor.txt pasta4\Flor.txt
+cd pasta3
+echo textinhooo> Gatinho.txt
+pause
+del Gatinho.txt
+cd ..
+cd pasta4
+echo textooo> coelho.txt
+copy coelho.txt coelhinho.txt
+type coelho.txt
+pause
+
